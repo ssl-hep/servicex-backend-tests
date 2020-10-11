6ea3220e5fed1e557df0481691b216363eff36d3
David Liu <dwliu@uw.edu>
2020-10-11 10:46:15 -0700

    update to rc3 compatibility

--- docker_tests.sh
+++ docker_tests.sh
@@ -4,26 +4,46 @@
 
 # Here, we will execute the tests by building a .servicex file with the correct backend and credentials, running the tests, and then deleting the .servicex file.
 
-if [[ $1 = 'uproot' ]] || [[ $1 = 'Uproot' ]];
+# First, if the user did not specify a custom endpoint, we use the defaults:
+if [[-z $4]];
 then
-    printf "api_endpoint:\n  endpoint: http://test1-uproot-servicex.uc.ssl-hep.org/\n  email: $2\n  password: $3" > ./.servicex
-    pytest tests/test_uproot_functions.py
-    rm ./.servicex
-elif [[ $1 = 'xaod' ]] || [[ $1 = 'xAOD' ]];
-then
-    printf "api_endpoint:\n  endpoint: http://xaod-servicex.uc.ssl-hep.org/\n  email: $2\n  password: $3" > ./.servicex
-    pytest tests/test_xAOD_functions.py
-    rm ./.servicex
-elif [[ $1 = 'stress' ]] || [[ $1 = 'Stress' ]];
-then
-    printf "api_endpoint:\n  endpoint: http://xaod-servicex.uc.ssl-hep.org/\n  email: $2\n  password: $3" > ./.servicex
-    pytest --stress tests/test_large_xAOD_datasets.py
-    rm ./.servicex
-elif [[ ${1:0:4} = 'http' ]];
-then
-    printf "api_endpoint:\n  endpoint: $1\n  email: $2\n  password: $3" > ./.servicex
-    pytest
-    rm ./.servicex
-else;
-    echo 'Invalid backend specified. Please enter a type of test you would like to run: xAOD, uproot, or stress, or specify a backend using its http address.'
+    if [[ $1 = 'uproot' ]] || [[ $1 = 'Uproot' ]];
+    then
+        printf "api_endpoint:\n  endpoint: http://test1-uproot-servicex.uc.ssl-hep.org/\n  token: $2\n  type: uproot" > ./.servicex
+        pytest tests/test_uproot_functions.py
+        rm ./.servicex
+    elif [[ $1 = 'xaod' ]] || [[ $1 = 'xAOD' ]];
+    then
+        printf "api_endpoint:\n  endpoint: https://xaod.servicex.ssl-hep.org/\n  token: $2\n  type: xaod" > ./.servicex
+        pytest tests/test_xAOD_functions.py
+        rm ./.servicex
+    elif [[ $1 = 'stress_xAOD' ]] || [[ $1 = 'Stress_xAOD' ]];
+    then
+        printf "api_endpoint:\n  endpoint: https://xaod.servicex.ssl-hep.org/\n  token: $2\n  type: xaod" > ./.servicex
+        pytest --stress tests/test_large_xAOD_datasets.py
+        rm ./.servicex
+    else
+        echo 'Invalid backend specified. Please enter a type of test you would like to run: xAOD, uproot, or stress, or specify a backend using its http address.'
+    fi
+
+# if the user specified a custom endpoint, run the custom endpoint
+else
+    if [[ $1 = 'uproot' ]] || [[ $1 = 'Uproot' ]];
+    then
+        printf "api_endpoint:\n  endpoint: $3\n  token: $2\n  type: uproot" > ./.servicex
+        pytest tests/test_uproot_functions.py
+        rm ./.servicex
+    elif [[ $1 = 'xaod' ]] || [[ $1 = 'xAOD' ]];
+    then
+        printf "api_endpoint:\n  endpoint: $3\n  token: $2\n  type: xaod" > ./.servicex
+        pytest tests/test_xAOD_functions.py
+        rm ./.servicex
+    elif [[ $1 = 'stress_xAOD' ]] || [[ $1 = 'Stress_xAOD' ]];
+    then
+        printf "api_endpoint:\n  endpoint: $3/\n  token: $2\n  type: xaod" > ./.servicex
+        pytest --stress tests/test_large_xAOD_datasets.py
+        rm ./.servicex
+    else
+        echo 'Invalid backend specified. Please enter a type of test you would like to run: xAOD, uproot, or stress_xAOD.'
+    fi
 fi
