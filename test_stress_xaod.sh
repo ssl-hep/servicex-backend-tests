27de448efd38a56526cb60a28e487a14253dcb02
David Liu <dwliu@uw.edu>
2020-10-14 12:46:52 -0700

    updates for RC3 compatibility

--- test_stress_xaod.sh
+++ test_stress_xaod.sh
@@ -1,6 +1,6 @@
 # A simple shell script that will generate a .servicex file and automatically run it, primarily meant for GitHub actions.
 # David Liu, 21 September 2020, Seattle, WA.
 
-printf "api_endpoint:\n  endpoint: https://xaod.servicex.ssl-hep.org/\n  email: $1\n  password: $2" > ./.servicex
+printf "api_endpoints:\n  - endpoint: https://xaod.servicex.ssl-hep.org/\n    email: $1\n    password: $2" > ./.servicex
 pytest --stress tests/test_large_xAOD_datasets.py
 rm ./.servicex
--- test_xaod.sh
+++ test_xaod.sh
@@ -1,6 +1,6 @@
 # A simple shell script that will generate a .servicex file and automatically run it, primarily meant for GitHub actions.
 # David Liu, 21 September 2020, Seattle, WA.
 
-printf "api_endpoint:\n  endpoint: https://xaod.servicex.ssl-hep.org/\n  token: $1\n  type: xaod" > ./.servicex
+printf "api_endpoints:\n  - endpoint: https://xaod.servicex.ssl-hep.org/\n    token: $1\n    type: xaod" > ./.servicex
 pytest tests/test_xAOD_functions.py
 rm ./.servicex
