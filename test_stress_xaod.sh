60bc97f6af19f87d789acb9d8f83b0503b0cb769
Ilija Vukotic <ivukotic@cern.ch>
2021-01-13 11:51:13 -0600

    servicex.yaml change

--- test_stress_xaod.sh
+++ test_stress_xaod.sh
@@ -1,6 +1,6 @@
 # A simple shell script that will generate a servicex.yaml file and automatically run it, primarily meant for GitHub actions.
 # David Liu, 21 September 2020, Seattle, WA.
 
-printf "api_endpoints:\n  - endpoint: https://servicex.vukotic.me/\n" > ./servicex.yaml
+printf "api_endpoints:\n  - endpoint: https://xaod.servicex.ssl-hep.org/\n    email: $1\n    password: $2" > ./servicex.yaml
 pytest --stress tests/test_large_xAOD_datasets.py
 rm ./servicex.yaml
--- test_xaod.sh
+++ test_xaod.sh
@@ -1,6 +1,6 @@
 # A simple shell script that will generate a servicex.yaml file and automatically run it, primarily meant for GitHub actions.
 # David Liu, 21 September 2020, Seattle, WA.
 
-printf "api_endpoints:\n  - endpoint: https://servicex.vukotic.me/\n    type: xaod" > ./servicex.yaml
+printf "api_endpoints:\n  - endpoint: https://xaod.servicex.ssl-hep.org/\n    token: $1\n    type: xaod" > ./servicex.yaml
 pytest tests/test_xAOD_functions.py
 rm ./servicex.yaml
