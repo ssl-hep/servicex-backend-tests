4343f7534ca98b1fa8eff9c2a34c8c3f393fb776
Ilija Vukotic <ivukotic@cern.ch>
2022-02-15 11:29:36 -0600

    now testing testing1 and testing2 instances.

--- test_uproot.sh
+++ test_uproot.sh
@@ -1,6 +1,6 @@
 # A simple shell script that will generate a servicex.yaml file and automatically run it, primarily meant for GitHub actions.
 # David Liu, 21 September 2020, Seattle, WA.
 
-printf "api_endpoints:\n  - endpoint: https://uproot-atlas.servicex.ssl-hep.org/\n    token: $1\n    type: uproot" > ./servicex.yaml
+printf "api_endpoints:\n  - endpoint: https://servicex-release-testing-1.servicex.ssl-hep.org/\n    token: $1\n    name: uproot" > ./servicex.yaml
 pytest -s tests/test_uproot_functions.py
 rm ./servicex.yaml
