043c9be18e0233fed9d5a71b801098a72ab57002
Ilija Vukotic <ivukotic@cern.ch>
2022-11-03 11:54:09 -0500

    force action

--- test_stress_xaod.sh
+++ test_stress_xaod.sh
@@ -1,5 +1,6 @@
 # A simple shell script that will generate a servicex.yaml file and automatically run it, primarily meant for GitHub actions.
 # David Liu, 21 September 2020, Seattle, WA.
+# Ilija Vukotic, 2022, Chicago, IL.
 
 printf "api_endpoints:\n  - endpoint: https://xaod.servicex.ssl-hep.org/\n    email: $1\n    password: $2" > ./servicex.yaml
 pytest --stress tests/xAOD/test_large_xAOD_datasets.py
